7de464a1f116a8c54df09a908df8d78d8702ddb2
StopwatchCAMEL <Cody.lin@live.com>
2016-09-08 08:20:28 -0700

    cats

--- APLesson_01/Calculator.java
+++ APLesson_01/Calculator.java
@@ -8,6 +8,12 @@ public class Calculator
 		int	num2;
 		int num3;
 		int total; 
+		String name;
+		int housenum;
+		String street;
+		String city;
+		int zipcode;
+		//Complex Calculation
 			System.out.println("What is the length?");
 			num1 = user_input.nextInt();
 			System.out.println("What is the height?");
@@ -15,13 +21,24 @@ public class Calculator
 			System.out.println("What is the width?");
 			num3 = user_input.nextInt();
 			total = (2*num1*num3+2*num1*num2+2*num2*num3);
-				System.out.println("The surface area of your rectangle is" );
-				System.out.println(total);
-				System.out.print(num1);
-				System.out.print(" and");
-				System.out.print(num2);
-				System.out.print(" would produce");
-				System.out.print(num1*num2);
-		
+			System.out.println("The surface area of your rectangle is" );
+			System.out.println(total);
+			//Multiplication
+				System.out.println(num1 + " and " + num2 + " would produce " + num1*num2);
+					//Concatenation
+					System.out.println("What is your name? ");
+					name = user_input.next();
+					System.out.println("What is your house number?");
+					housenum = user_input.nextInt();
+					System.out.println("What is your street name");
+					street = user_input.next();
+					System.out.println("What city do you live in?");
+					city = user_input.next();
+					System.out.println("What is your zipcode?");
+					zipcode = user_input.nextInt();
+					System.out.print("Your name is. " + name);
+					System.out.println(" You live at");
+					System.out.print(housenum + " " + street + " " + city + " " + zipcode);
+				
 	}
 }
