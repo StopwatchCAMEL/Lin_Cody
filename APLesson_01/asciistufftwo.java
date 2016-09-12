72ae194358e26506b3154a3485d82c18e75150b0
StopwatchCAMEL <Cody.lin@live.com>
2016-09-12 08:10:06 -0700

    Stuff

---
+++ APLesson_01/asciistufftwo.java
@@ -0,0 +1,13 @@
+public class asciistufftwo
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" _____     ____");
+		System.out.println(" /        \\     |    o |  ");
+		System.out.println("|           |/  ___  \\|  ");
+		System.out.println("|_________/ ");
+		System.out.println("|_|_| |_|_| ");
+		
+		
+	}
+}	
